1df5c08c245d7067a5af02b9234d67a37dad4297
Shaida92 <nazirshaheeda@gmail.com>
2026-07-16 15:59:01 -0400

    Remove duplicate SQL query files

--- Sql/SQLQuery7.sql
+++
@@ -1,9 +0,0 @@
-USE NetherlandsHousingAnalytics;
-
-CREATE TABLE staging.population_summary (
-    year_value INT,
-    region_name NVARCHAR(100),
-    total_population INT,
-    male_population INT,
-    female_population INT
-);
